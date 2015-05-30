3423a91a3f82cb6cc8dddbd11f07faf7d682914e
kbinani <kbinani.bt@gmail.com>
2015-05-30 20:25:08 +0900

    Move "vec4" struct definition into "Colormap" class

--- include/colormap/colormap.h
+++ include/colormap/colormap.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "./vec4.h"
+#include <cassert>
 #include <cmath>
 #include <vector>
 #include <string>
@@ -26,12 +26,103 @@ public:
 	virtual std::string getCategory() const = 0;
 
 protected:
+	struct vec4
+	{
+		vec4(float a0, float a1, float a2, float a3)
+			: x(a0)
+			, y(a1)
+			, z(a2)
+			, w(a3)
+		{
+		}
+
+		float operator[](size_t index) const
+		{
+			assert(index < 4);
+			if (index < 2) {
+				if (index < 1) {
+					return x;
+				} else {
+					return y;
+				}
+			} else {
+				if (index < 3) {
+					return z;
+				} else {
+					return w;
+				}
+			}
+		}
+
+		float& operator[](size_t index)
+		{
+			assert(index < 4);
+			if (index < 2) {
+				if (index < 1) {
+					return x;
+				} else {
+					return y;
+				}
+			} else {
+				if (index < 3) {
+					return z;
+				} else {
+					return w;
+				}
+			}
+		}
+
+		bool operator==(vec4 const& o) const
+		{
+			return x == o.x && y == o.y && z == o.z && w == o.w;
+		}
+
+		vec4 operator*(float v) const
+		{
+			return vec4(r * v, g * v, b * v, a * v);
+		}
+
+		vec4 operator+(vec4 const& v) const
+		{
+			return vec4(r + v.r, g + v.g, b + v.b, a + v.a);
+		}
+
+		std::string to_string() const
+		{
+			return
+				std::string("{") +
+				std::to_string(x) + std::string(",") +
+				std::to_string(y) + std::string(",") +
+				std::to_string(z) + std::string(",") +
+				std::to_string(w) +
+				std::string("}");
+		}
+
+		union {
+			float r;
+			float x;
+		};
+		union {
+			float g;
+			float y;
+		};
+		union {
+			float b;
+			float z;
+		};
+		union {
+			float a;
+			float w;
+		};
+	};
+
 	class WrapperBase
 	{
 	public:
 		virtual ~WrapperBase()
 		{}
 
+	protected:
 		template <class Value, class MinMax>
 		typename std::common_type<Value, MinMax>::type clamp(Value v, MinMax min, MinMax max) const
 		{
