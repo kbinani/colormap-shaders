e042cba391d32b8187535e66e9a254200224814e
kbinani <kbinani.bt@gmail.com>
2018-06-30 18:10:54 +0900

    Change extension to ".txt"

--- tool/transcode.sh
+++ tool/transcode.sh
@@ -8,7 +8,7 @@ glsl:frag"
 	for SHADER in $SHADERS; do
 		TYPE=$(echo $SHADER | cut -d: -f1)
 		EXT=$(echo $SHADER | cut -d: -f2)
-		for FILE in $(find . -name '*\.frag'); do
+		for FILE in $(find . -name '*\.txt'); do
 			NAME=$(basename "$FILE" | cut -d. -f1)
 			bash ../tool/transcode/glsl_to_${TYPE}.sh "$FILE" "../shaders/${TYPE}/${NAME}.${EXT}"
 		done
