ec4f7c1befacb22f01e47f67debaa91b37e9172f
kbinani <kbinani.bt@gmail.com>
2018-07-01 16:48:09 +0900

    Cleanup stdout of scripts

--- tool/create_samples.sh
+++ tool/create_samples.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+function exec {
+	echo -e "\033[2m$@\033[0m"
+	$@
+}
+
 (
 	cd "$(dirname "$0")/create_samples"
-	mkdir -p ../../samples
+	exec pwd
+	exec mkdir -p ../../samples
 	(
-		cd build
-		cmake ..
-		cmake --build .
+		exec cd build
+		exec cmake ..
+		exec cmake --build .
 	)
+
+	echo find ../../shaders/glsl/*.frag | grep -v gnuplot | xargs -I{} -L1 bash -c 'build/create_samples {} ../../samples/$(basename "{}" | cut -d. -f1).png'
 	find ../../shaders/glsl/*.frag | grep -v gnuplot | xargs -I{} -L1 bash -c 'build/create_samples {} ../../samples/$(basename "{}" | cut -d. -f1).png'
+
 	for ARGS in 21_22_23 23_28_3 30_31_32 33_13_10 34_35_36 3_11_6 7_5_15; do
 		A0=$(echo $ARGS | cut -f1 -d_)
 		A1=$(echo $ARGS | cut -f2 -d_)
 		A2=$(echo $ARGS | cut -f3 -d_)
-		build/create_samples ../../shaders/glsl/gnuplot.frag ../../samples/gnuplot_$ARGS.png $A0 $A1 $A2
+		exec build/create_samples ../../shaders/glsl/gnuplot.frag ../../samples/gnuplot_$ARGS.png $A0 $A1 $A2
 	done
 )
--- tool/generate_and_merge.sh
+++ tool/generate_and_merge.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
 
+function exec {
+	echo -e "\033[2m$@\033[0m"
+	$@
+}
+
 (
 	cd "$(dirname "$0")/.."
 
-	rm -rf shaders include/colormap/private samples
+	exec pwd
+	exec git submodule update --init --recursive
+	exec rm -rf shaders include/colormap/private samples
 
-	bash tool/transcode.sh
-	bash tool/create_c++_header.sh
-	bash tool/create_samples.sh
+	exec bash tool/transcode.sh
+	exec bash tool/create_c++_header.sh
+	exec bash tool/create_samples.sh
 
 	COMMIT_ID=$(git rev-parse HEAD)
 
 	BRANCH_NAME=$(date "+%s")
-	git checkout --orphan $BRANCH_NAME
-	ls -a1 | grep -v include | grep -v shaders | grep -v samples | grep -v '^.$' | grep -v '^..$' | grep -v '^.git$'| xargs git rm -rf
-	git add -A include shaders samples
-	git commit -m "Generated: $COMMIT_ID"
+	exec git checkout --orphan $BRANCH_NAME
 
-	git checkout master
+	exec git submodule deinit tool/create_samples/ext/pngpp
+	exec git rm -rf tool/create_samples/ext/pngpp
+
+	echo ls -a1 | grep -v include | grep -v shaders | grep -v samples | grep -v '^.$' | grep -v '^..$' | grep -v '^.git$'| xargs git rm -rf
+	ls -a1 | grep -v include | grep -v shaders | grep -v samples | grep -v '^.$' | grep -v '^..$' | grep -v '^.git$'| xargs git rm -rf
+
+	exec git add -A include shaders samples
+	
+	echo git commit -m "Generated: $COMMIT_ID"
+	git commit -m "Generated: $COMMIT_ID"
+	
+	exec git checkout master
+
+	echo git merge --allow-unrelated-histories $BRANCH_NAME -m "Generated: $COMMIT_ID"
 	if git merge --allow-unrelated-histories $BRANCH_NAME -m "Generated: $COMMIT_ID"; then
-		git reset --hard HEAD^
-		git checkout $COMMIT_ID
+		echo "There is no update. checkout to $COMMIT_ID"
+		exec git reset --hard HEAD^
+		exec git checkout $COMMIT_ID
 	else
-		git checkout --theirs .
-		git add -A .
+		exec git checkout --theirs .
+		exec git add -A .
+		
+		echo git commit -m "Generated: $COMMIT_ID"
 		git commit -m "Generated: $COMMIT_ID"
 	fi
 )
